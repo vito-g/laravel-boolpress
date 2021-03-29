5acfd8b8c0ed250f562ecf5d10e13945373d048f
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 22:59:21 +0200

    Web.php: Rotte alle index di Author e Post nel Crud. ESERCIZIO OK

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 //Creao la rotta relativa al metodo Index del Controller 'AuthorController':
-Route::get('/author', 'AuthorController@index');
+// Route::get('/author', 'AuthorController@index');
 
-Route::resource('/post', 'PostController');
+// Route::resource('/post', 'PostController');
 // La stringa, qui sopra, crea tutte le rotte, relative ai vari metodi (ancora vuoti) predisposti nel CRUD (che andrò, all'occorrenza, a definire) e dichiara il path di riferimento: /beers.
 
 //(-1)In alternativa, potrei scrivere:
@@ -27,3 +27,6 @@ Route::resource('/post', 'PostController');
 // Route::resource('/post',postController::class);
 
 // Si sfrutta, così, la proprietà class, molto utile , soprattutto, qualora in fase di digitazione del nome della classe lo si scriva in modo errato. Class, cioè, fa un match tra quello che scrivo e il nome delle classi che Laravel ha in memoria; restituendone eventualmente l'errore.
+
+Route::get('/author', 'PostController@indexAuthor');
+Route::get('/post', 'PostController@indexPost');
