14ee61266822412c4643cf52db82e4f5e6d18079
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 17:00:47 +0200

    Struttura Tab author_details Creata

--- database/migrations/2021_03_29_143745_create_author_details_table.php
+++ database/migrations/2021_03_29_143745_create_author_details_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+//N.B.: Questa Tabella conterrà la FK che rimanderà all'id dell'autore
 class CreateAuthorDetailsTable extends Migration
 {
     /**
@@ -15,7 +15,18 @@ class CreateAuthorDetailsTable extends Migration
     {
         Schema::create('author_details', function (Blueprint $table) {
             $table->id();
+            // Creiamo una colonna BigInteger che riporti quella che sarà la ForeignKey(FK), 'author_id', nella tabella 'author_details'. Essa richiamerà l'id (PK) dell'autore (vedi più in basso(*)):
+            $table->unsignedBigInteger('author_id');
+            //Creiamo, qui sotto, tutte le altre colonne della tabella:
+            $table->text('biography');
+            $table->string('website', 2048);
+            $table->string('picture', 2048);
+
             $table->timestamps();
+            //(*)Creiamo la relazione, di cui sopra:
+            $table->foreign('author_id')//Qui la FK presente in 'author_details'.
+            ->references('id')//Qui l'id (PK) nella tabella 'authors' cui la FK fa riferimento
+            ->on('authors');//Qui il nome della tabella contenente quella PK
         });
     }
 
