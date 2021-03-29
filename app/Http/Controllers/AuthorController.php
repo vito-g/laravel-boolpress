4754979199831841a39bb99bceff3adc6567b771
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 21:31:47 +0200

    AuthorController: Return view Index

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -9,8 +9,10 @@ class AuthorController extends Controller
 {
   //Creiamo una function index:
   public function index() {
-    // dump('Prova');//Test di Collegamento Rotta.
+    // dd('Prova');//Test di Collegamento Rotta.
     //Creo una variabile in cui salvare tutte le righe della tabella 'authors' del mio Database 'boolpress':
     $authors = Author::all();
+    // dd($authors);
+    return view('author.index', compact('authors'));
   }
 }
