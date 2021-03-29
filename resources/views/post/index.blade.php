2095adc6a44aa0f8b987c03c6c41b33a0a8f8794
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 22:42:07 +0200

    Post: HTML di Cornice + Index.blade con foreach

---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,28 @@
+@extends('post.base')
+
+@section ('title', 'index')
+
+@section('content')
+
+  <table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+        <th scope="col">Author</th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach( $posts as $key => $post)
+        <tr>
+          <th scope="row">{{$post->id}}</th>
+          <td>{{$post->title}}</td>
+          <td>{{$post->body}}</td>
+          <td>{{$post->author->lastname}}</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+
+@endsection
