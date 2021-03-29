11dbc0cd773e6dc3e876a2ad7beeffb477603fe0
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 17:18:25 +0200

    Commenti

--- app/Http/Controllers/AuthorController.php
+++ app/Http/Controllers/AuthorController.php
@@ -3,12 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Author;
+use App\Author;//Inserito per consentire al controller di comunicare col Database a mezzo Model Author.
 
 class AuthorController extends Controller
 {
   //Creiamo una function index:
   public function index() {
-    dump('Prova');
+    dump('Prova');//Test di collegamento rotta
   }
 }
