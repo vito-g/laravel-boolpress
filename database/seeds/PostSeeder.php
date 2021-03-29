8babdc1e1b26f7b52d8f3747e8f6b4dff39f5cb0
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 20:18:06 +0200

    PostSeeder OK - Segue db:seed per Popolamento Tab Database

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -15,6 +15,29 @@ class PostSeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        //
+      for ($i = 0; $i < 10: $i++) {
+
+        $author = new Author();
+        $author->name = $faker->firstName();
+        $author->lastname = $faker->lastName();
+        $author->email = $faker->email();
+        $author->save();
+
+        $authorDetail = new AuthorDetail();
+        $authorDetail->biography = $faker->text();
+        $authorDetail->website = $faker->url();
+        $authorDetail->picture = 'https://picsum.photos/seed/' . rand(0, 1000) . '/200/300';
+
+        $author->detail()->save($authorDetail);
+
+        for ($j = 0; $j < rand (1, 4); $j++) {
+          $post = new Post();
+          $post->title = $faker->text(30);
+          $post->body = $faker->text(1200);
+
+          $author->post()->save($post);
+        }
+      }
+
     }
 }
