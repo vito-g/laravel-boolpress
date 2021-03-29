7fc5f78b180376b30f232f5926171d645f9b9928
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 18:52:24 +0200

    Struttura Tab posts Creata

--- database/migrations/2021_03_29_162129_create_posts_table.php
+++ database/migrations/2021_03_29_162129_create_posts_table.php
@@ -15,7 +15,17 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            // Creiamo una colonna BigInteger che riporti quella che sarà la ForeignKey(FK), 'author_id', nella tabella 'posts'. Essa richiamerà l'id (PK) dell'autore (vedi più in basso(*)):
+            $table->unsignedBigInteger('author_id');
+            //Creiamo, qui sotto, tutte le altre colonne della tabella:
+            $table->string('title', 20);
+            $table->text('body');
+
             $table->timestamps();
+            //(*)Creiamo la Relazione di cui sopra (visibile, poi, nel 'Designer' del Database, una volta lanciata la migrate):
+            $table->foreign('author_id')//Qui la FK presente in 'author_details'.
+            ->references('id')//Qui l'id (PK) nella tabella 'authors' cui la FK fa riferimento
+            ->on('authors');//Qui il nome della tabella contenente quella PK
         });
     }
 
