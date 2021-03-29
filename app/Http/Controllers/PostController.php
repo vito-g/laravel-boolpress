4a4a9117a5aadf2dbc8f0247511079c9e80c49ff
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 22:10:07 +0200

    Crud: Inserito Use del Model Author

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Author;//Inserito per consentire al presente Controller di comunicare col Database a mezzo Model 'Author'.
 
 class PostController extends Controller
 {
