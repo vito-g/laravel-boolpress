7b35d33f79800f025a60948816b64867037c3de1
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 19:25:57 +0200

    MAPPING: Tab Secondaria To Tab Principale - Aggiornamento e Commenti

--- app/Author.php
+++ app/Author.php
@@ -12,9 +12,13 @@ class Author extends Model
     }
     //In altri termini, ogni qualvolta dovremo mappare una relazione tra due o più tabelle dovremo creare una MAPPING tra la tabella secondaria (qui chiamata, 'authors') e la primaria (qui, nel progetto, chiamata 'author_details'). Procederemo all'inverso nell'altro Model.
 
-    // Qui vado ad inserire il TIPO di  relazione (OneToMany) esistente tra la tab 'authors', relativa a questo Model, e la tab 'posts'(contenete, la FK collegata all'id di 'authors'). Partendo dal presente Model 'Author' dobbiamo arrivare al Model 'Post'. Definisco, dunque, una FX con un nome che identifichi la tab ('posts') relativa all'altro Model (il Post).
-    public function posts() {
+    //Questa sopra - COME POSSO LEGGERLA: Un autore può avere un solo dettaglio (inteso come profilo).
+
+    // Qui vado ad inserire il TIPO di  relazione (OneToMany: un author ha tanti post) esistente tra la tab 'authors', relativa a questo Model, e la tab 'posts'(contenete, la FK collegata all'id di 'authors'). Partendo dal presente Model 'Author' dobbiamo arrivare al Model 'Post'. Definisco, dunque, una FX con un nome che identifichi la tab ('posts') relativa all'altro Model (il Post).
+    public function post() {
       return $this->hasMany('App\Post');//'hasOne' indica il tipo di relazione 1 ad 1
     }
     //In altri termini, ogni qualvolta dovremo mappare una relazione tra due o più tabelle dovremo creare una MAPPING tra la tabella secondaria (qui chiamata, 'authors') e la primaria (qui, nel progetto, chiamata 'posts'). Procederemo all'inverso nell'altro Model; cioè, in Post.
 }
+
+//Quest'ultima - COME POSSO LEGGERLA: Un autore può avere tanti post.
