cd294a95b4abbd047c997e894270a0512c9ac40e
Vito Gentile <gentilevito1076@gmail.com>
2021-03-29 19:07:29 +0200

    MAPPING: Tab Secondaria 'authors' To altra Tab Principale 'posts'

--- app/Author.php
+++ app/Author.php
@@ -6,9 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    // Qui vado ad inserire il TIPO di  relazione esistente tra la tab 'authors', relativa a questo Model, e la tab 'author_details'(contenete, la FK collegata all'id di 'authors'). Partendo dal presente Model 'Author' dobbiamo arrivare al Model 'AuthorDetail'. Definisco, dunque, una FX con un nome che identifichi la tab ('author_details') relativa all'altro Model (l'AuthorDetail).
+    // Qui vado ad inserire il TIPO di  relazione (OneToOne) esistente tra la tab 'authors', relativa a questo Model, e la tab 'author_details'(contenete, la FK collegata all'id di 'authors'). Partendo dal presente Model 'Author' dobbiamo arrivare al Model 'AuthorDetail'. Definisco, dunque, una FX con un nome che identifichi la tab ('author_details') relativa all'altro Model (l'AuthorDetail).
     public function detail() {
       return $this->hasOne('App\AuthorDetail');//'hasOne' indica il tipo di relazione 1 ad 1
     }
-    //In altri termini, ogni qualvolta dovremo mappare una relazione tra due o più tabelle dovremo creare una MAPPING tra la tabella secondaria (qui chiamata, 'authors') e la primaria (qui, 'author_details'). Procederemo all'inverso nell'altro Model.
+    //In altri termini, ogni qualvolta dovremo mappare una relazione tra due o più tabelle dovremo creare una MAPPING tra la tabella secondaria (qui chiamata, 'authors') e la primaria (qui, nel progetto, chiamata 'author_details'). Procederemo all'inverso nell'altro Model.
+
+    // Qui vado ad inserire il TIPO di  relazione (OneToMany) esistente tra la tab 'authors', relativa a questo Model, e la tab 'posts'(contenete, la FK collegata all'id di 'authors'). Partendo dal presente Model 'Author' dobbiamo arrivare al Model 'Post'. Definisco, dunque, una FX con un nome che identifichi la tab ('posts') relativa all'altro Model (il Post).
+    public function posts() {
+      return $this->hasMany('App\Post');//'hasOne' indica il tipo di relazione 1 ad 1
+    }
+    //In altri termini, ogni qualvolta dovremo mappare una relazione tra due o più tabelle dovremo creare una MAPPING tra la tabella secondaria (qui chiamata, 'authors') e la primaria (qui, nel progetto, chiamata 'posts'). Procederemo all'inverso nell'altro Model; cioè, in Post.
 }
