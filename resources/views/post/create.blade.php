406331dbb77d0f4e6a9a37036fc227b025da96e2
Vito Gentile <gentilevito1076@gmail.com>
2021-03-30 16:00:44 +0200

    Create: Inserita Textarea presa da Bootstrap e Rimaneggiata

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,9 +1,24 @@
 @extends('post.base')
 
-@section ('title', 'create')
+@section ('title', 'create-post')
 
 @section('content')
 
-
+  <form>
+    <div class="form-group">
+      <label for="title">Title-Post</label>
+      <input type="text" class="form-control" id="title" name="title" placeholder="title">
+    </div>
+    <div class="form-group">
+      <label for="body">Body-Post</label>
+      <textarea class="form-control" id="body" name="body" rows="24"></textarea>
+    </div>
+    {{-- <div class="form-group">
+      <label for="exampleFormControlSelect2">Example multiple select</label>
+      <select multiple class="form-control" id="exampleFormControlSelect2">
+        <option>1</option>
+      </select>
+    </div> --}}
+  </form>
 
 @endsection
