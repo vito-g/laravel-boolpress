cd6e2d5941fd24da07a7fb6802030252bbf34338
Vito Gentile <gentilevito1076@gmail.com>
2021-04-03 12:42:17 +0200

    PostCreated: Inserito lo use del Post

--- app/Mail/PostCreated.php
+++ app/Mail/PostCreated.php
@@ -7,6 +7,8 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
 use Illuminate\Queue\SerializesModels;
 
+use App\Post;
+
 class PostCreated extends Mailable
 {
     use Queueable, SerializesModels;
