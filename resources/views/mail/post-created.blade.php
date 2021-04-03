926ed0b6dd445e6fedc694d5397823115355b9f3
Vito Gentile <gentilevito1076@gmail.com>
2021-04-03 13:19:34 +0200

    post-created: aggiornato per stampa info nella mail

--- resources/views/mail/post-created.blade.php
+++ resources/views/mail/post-created.blade.php
@@ -1,1 +1,4 @@
-Questa è la mail che avvisa di un nuovo post creato
+E' stato creato un nuovo post dal titolo {{ $post -> Title}} alle {{ $post -> created_at}}
+<br><br>
+Questo è il suo contenuto: <br><br>
+{{ $post -> body}}
