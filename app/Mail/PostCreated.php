63253113430bce8ea564b508edaac7943a390c98
Vito Gentile <gentilevito1076@gmail.com>
2021-04-03 11:47:42 +0200

    PostCreated: Nome template base email aggiornato

--- app/Mail/PostCreated.php
+++ app/Mail/PostCreated.php
@@ -28,6 +28,6 @@ class PostCreated extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->view('mail.post-created');
     }
 }
