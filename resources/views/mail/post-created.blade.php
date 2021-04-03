c8d0cbec694f7dea524dc0b6c08e1f50537b9756
Vito Gentile <gentilevito1076@gmail.com>
2021-04-03 13:23:01 +0200

    aggiornamento

--- resources/views/mail/post-created.blade.php
+++ resources/views/mail/post-created.blade.php
@@ -1,4 +1,4 @@
-E' stato creato un nuovo post dal titolo {{ $post -> Title}} alle {{ $post -> created_at}}
+E' stato creato un nuovo post dal titolo {{ $post->title}} alle {{ $post->created_at}}
 <br><br>
 Questo è il suo contenuto: <br><br>
-{{ $post -> body}}
+{{ $post->body}}
